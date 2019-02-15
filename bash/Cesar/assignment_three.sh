bae8cad7e3cb76535cbae08415d1edf6aaade694
Cesar Omar Sanchez <30625358+cesaromar1973@users.noreply.github.com>
2019-02-15 15:11:27 -0500

    Add files via upload

---
+++ bash/Cesar/assignment_three.sh
@@ -0,0 +1,22 @@
+Problem 1:
+
+Hello Professor, Not exactly sure what homework three entails. However, I will give it a shot.
+type: crontab -e
+
+Then add the follwoing string of code 
+For example, this will execute a script every day at 9:00AM
+
+* * * * * /path/to/command arg1 arg2
+
+0 9 * * * /home/MyUser/MyCrons/myScript.sh
+
+Now to make this script run from a particular user, edit the following:
+
+crontab -l -u USERNAME (USERNAME HERE OF THE PERSON YOU ARE TRYING CYBER ATTACK)
+
+
+Problem 2: 
+
+I Believe we already Clond and submit pull requests. I'm currently reading the Git Book, THERE IS ALOT TO COVER!
+WOW!
+
